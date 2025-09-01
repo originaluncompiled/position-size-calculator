05cd4426874030fec7da3373f818820e9b85d8a2
originaluncompiled <carstendold1@gmail.com>
2025-09-01 18:03:35 +0200

    added responsiveness - actually tho

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" className="bg-slate-950">
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import CalculatorCard from "@/components/CalculatorCard";
 
 export default function Home() {
 	return (
-		<main className="flex flex-row h-screen w-screen items-center justify-center bg-slate-950">
-			<CalculatorCard assetName="US500" assetColor="bg-red-500"/>
+		<main className="flex flex-row h-screen w-full items-center justify-center bg-slate-950">
+			<CalculatorCard/>
 		</main>
 	);
 }
