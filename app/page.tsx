3987c4a9ddd8d6b6fd05eeb62a7f0cd3f10f2925
originaluncompiled <carstendold1@gmail.com>
2025-09-01 18:13:26 +0200

    fixed responsiveness_final_final

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import CalculatorCard from "@/components/CalculatorCard";
 
 export default function Home() {
 	return (
-		<main className="flex flex-row h-svh w-full items-center justify-center bg-slate-950">
+		<main className="flex flex-row h-screen w-full items-center justify-center bg-slate-950">
 			<CalculatorCard/>
 		</main>
 	);
