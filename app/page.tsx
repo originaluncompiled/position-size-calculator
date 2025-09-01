544e92adc4d2bd9934f57be27a18767a41a20a24
originaluncompiled <carstendold1@gmail.com>
2025-09-01 18:37:26 +0200

    i gave up

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import CalculatorCard from "@/components/CalculatorCard";
 
 export default function Home() {
 	return (
-		<main className="flex flex-row w-full h-dvh items-center justify-center bg-slate-950">
+		<main className="flex flex-row w-full h-screen overflow-hidden items-center justify-center bg-slate-950">
 			<CalculatorCard/>
 		</main>
 	);
